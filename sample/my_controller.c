08aeba2add6020622942b2d7cf63a0d3a6c1ef65
chunmingYang <95987545+chunmingYang@users.noreply.github.com>
2022-02-25 14:06:09 -0600

    Add files via upload

---
+++ sample/my_controller.c
@@ -0,0 +1,43 @@
+double setpt[nact] = {0};
+
+void init_controller()
+{
+    int i;
+    double target[]={0,0.9,-2.2,   //FR B
+                     0,0.9,-2.2,   //FL A
+                     0,0.9,-2.2,   //RR A
+                     0,0.9,-2.2};  //RL B
+    
+    for (i=0;i<nact;i++)
+    {
+        setpt[i]=target[i];
+    }
+}
+
+void control_loop(const mjModel* m, mjData* d,
+                  double q[nact], double u[nact], double base_x[3], double base_quat[4],
+                  double base_angvel[3], double base_linacc[3], double base_linvel[3],
+                  double pos_FR_shoulder[3], double pos_FL_shoulder[3], double pos_RR_shoulder[3], double pos_RL_shoulder[3],
+                  double pos_FR_elbow[3], double pos_FL_elbow[3], double pos_RR_elbow[3], double pos_RL_elbow[3],
+                  double pos_FR_foot[3], double pos_FL_foot[3], double pos_RR_foot[3], double pos_RL_foot[3], double robot_com[3], double tau[nact], int flag, int state, double t)
+{
+    int i;
+
+    if (state == 1)
+    {
+        double target[]={0,0.9,-2.2,   //FR B
+                         0,0.9,-2.2,   //FL A
+                         0,0.9,-2.2,   //RR A
+                         0,0.9,-2.2};  //RL B
+
+        for (i=0;i<nact;i++)
+        {
+            setpt[i]=target[i];
+        }
+
+        for (i=0; i<nact; i++)
+        {
+            tau[i] = -250*(q[i]-setpt[i])-10*u[i];
+        }
+    }     
+}
